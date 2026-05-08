022e53b40192d4e33316e530b5be5918594c4d4a
mekeirish <mmm1747lb@gmail.com>
2026-05-08 14:10:44 +0200

    Refactor service worker code for brevity

--- sw.js
+++ sw.js
@@ -1,21 +1,4 @@
 const CACHE = 'v1';
-const assets = [
-    './',
-    './index.html',
-    './manifest.json',
-    './icon.png'
-];
-
-self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open(CACHE).then(cache => cache.addAll(assets))
-    );
-});
-
-self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
-        })
-    );
-});
+const assets = ['./', './index.html', './manifest.json', './icon.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(assets))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));

91c8b02e77ea57e502043bdd9866d3e329124fa7
mekeirish <mmm1747lb@gmail.com>
2026-05-08 15:39:35 +0200

    Upgrade cache version and fetch strategy in sw.js
    Updated service worker to use cache version v2 and modified fetch strategy to prioritize network requests.

--- sw.js
+++ sw.js
@@ -1,4 +1,14 @@
-const CACHE = 'v1';
+const CACHE = 'v2'; // Changé en v2
 const assets = ['./', './index.html', './manifest.json', './icon.png'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(assets))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(assets)));
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+    // Priorité au réseau pour voir les changements de signature
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request))
+    );
+});

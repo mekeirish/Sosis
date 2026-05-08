7450483b881818e7effa646a43b3f2de552bb912
mekeirish <mmm1747lb@gmail.com>
2026-05-08 13:42:02 +0200

    Refactor service worker to use assets array

--- sw.js
+++ sw.js
@@ -1,3 +1,21 @@
 const CACHE = 'v1';
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './manifest.json', './icon.png']))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const assets = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon.png'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE).then(cache => cache.addAll(assets))
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
